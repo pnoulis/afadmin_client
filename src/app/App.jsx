f6617788d46c688870fecfb77c1b617620058ab9
pnoul <pavlos.noulis@gmail.com>
2023-04-25 16:21:11 +0300

    Standard makefile targets, app imports afmachine

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,6 +1,9 @@
 import * as React from "react";
 import { Outlet } from "react-router-dom";
 import { Site } from "./Site.jsx";
+import { Afmachine } from "afmachine";
+
+Afmachine.init();
 
 function App() {
   return (

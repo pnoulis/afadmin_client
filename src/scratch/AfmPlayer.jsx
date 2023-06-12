560d8d41481a9456a0331cda41f14bf66abf98ef
pnoul <pavlos.noulis@gmail.com>
2023-06-12 17:01:30 +0300

    afmplayer demonstration

--- src/scratch/AfmPlayer.jsx
+++ src/scratch/AfmPlayer.jsx
@@ -1,12 +1,27 @@
 import * as React from "react";
-import { Player, useStateful } from "../afmachine/index.js";
+import { Player, useStateful, Wristband } from "../afmachine/index.js";
 import { AsyncState } from "../components/AsyncState.jsx";
 import {
   Player as PlayerStore,
   useContextPlayer,
 } from "../afm_components/players/index.js";
+import {
+  Wristband as WristbandParent,
+  useContextWristband,
+  StoreProvideWristband,
+  WidgetPairWristband,
+} from "../afm_components/wristbands/index.js";
 
 const pavlos = new Player();
+const pavlosWristband = new Wristband();
+
+function handleWristbandClick() {
+  pavlosWristband.togglePairing();
+}
+
+function handlePlayerClick() {
+  pavlos.register({});
+}
 
 function PlayerRegistration() {
   const { state, player, registrationState, onSubmitPlayerRegisterForm } =
@@ -15,10 +30,34 @@ function PlayerRegistration() {
     <div>
       <h1>player registration form</h1>
       <AsyncState state={registrationState}>
-        <div {...onSubmitPlayerRegisterForm()}>submit form</div>
-        <div>actual form</div>
+        <div
+          onClick={() => {
+            handlePlayerClick();
+          }}
+        >
+          submit form
+        </div>
         <div>{registrationState}</div>
       </AsyncState>
+    </div>
+  );
+}
+
+function WristbandChild({ ...props }) {
+  const { state, wristband } = useContextWristband();
+  return (
+    <div>
+      <br />
+      <h1>wristband pairing</h1>
+      <div
+        onClick={() => {
+          handleWristbandClick();
+        }}
+      >
+        pair wristband
+      </div>
+      <div>{state}</div>
+      <WidgetPairWristband />
     </div>
   );
 }
@@ -31,6 +70,9 @@ export default function AfmPlayer() {
         <PlayerStore player={pavlos}>
           <PlayerRegistration />
         </PlayerStore>
+        <WristbandParent wristband={pavlosWristband}>
+          <WristbandChild />
+        </WristbandParent>
       </div>
     </div>
   );

e3d697cc48febe494a810e23b79767ad3e58833f
pnoul <pavlos.noulis@gmail.com>
2023-07-12 17:05:41 +0300

    utilizing afadmin

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,8 @@ import { Afmachine } from 'afmachine';
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 
+// Afmachine.boot();
+
 ReactDOM.createRoot(document.getElementById("app-react-root")).render(
   <React.StrictMode>
     <div>hello</div>
